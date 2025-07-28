fffcab0e141685b89cc60d0b1d6934667c4526a1
Shrey K <srkkadia@gmail.com>
2025-07-28 14:04:30 +0530

    Intro

---
+++ Basics/Print_Scan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // int data;
+    // cout << "\nEnter any data: ";
+    // cin >> data;
+    // cout << "data: " << data << endl;
+    
+    // int a, b;
+    // cout << "Enter a, b: ";
+    // cin >> a >> b;
+    // cout << "A = " << a << ", B = " << b << endl;
+
+    return 0;
+}
