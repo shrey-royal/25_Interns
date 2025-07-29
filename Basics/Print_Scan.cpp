8b5f926d18f10d2072c45df30485a2591154a7ec
Shrey K <srkkadia@gmail.com>
2025-07-29 13:46:18 +0530

    Print and Scan, Array Tasks Added

--- Basics/Print_Scan.cpp
+++ Basics/Print_Scan.cpp
@@ -12,5 +12,36 @@ int main() {
     // cin >> a >> b;
     // cout << "A = " << a << ", B = " << b << endl;
 
+    // string datatype (derived)
+    string name;
+    // char name[10];  // length = 9 -> last character must be NULL
+
+    cout << "Enter your name: ";
+    // cin >> name;
+    getline(cin, name); //src (cin) -> dest (name)  // one line
+    // cin.getline(name, 10);
+
+    // cout << "Your name is " << name << endl;
+    // int i=0;
+    // while(name[i] != '\0') {
+    //     cout << name[i++] << ", ";
+    // }
+    // cout << endl;
+
+
+    for(char c : name) {    //for-each loop
+        cout << "'" << c << "', ";
+    }
+    cout << "\b\b \n";
+
+    int a[] = {1, 2, 3, 2, 1};
+
+    for (int i : a) {
+        cout << i << ", ";
+    }
+    cout << "\b\b \n";
+
+    cout << name << "'s length is " << name.length();
+
     return 0;
 }
